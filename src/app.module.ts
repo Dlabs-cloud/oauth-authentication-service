a9ec046f9a91d38555462008c7bc3ae94f27ae14
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-11-04 18:37:59 +0100

    feat/Installed independentModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ControllerModule } from './controller/controller.module';
+import { ConfModule } from './conf/conf.module';
+import { DaoModule } from './dao/dao.module';
+import { DomainModule } from './domain/domain.module';
+import { ServiceModule } from './service/service.module';
+import { ServiceImplModule } from './service-impl/service-impl.module';
+import { CoreModule } from './core/core.module';
+import { IntergrationModule } from './intergration/intergration.module';
 
 @Module({
-  imports: [],
+  imports: [ControllerModule,
+    ConfModule,
+    DaoModule,
+    DomainModule,
+    ServiceModule,
+    ServiceImplModule,
+    CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
