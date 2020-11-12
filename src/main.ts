e0adc190aced9b7ba41f6734a6a86fe9999737f0
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-11-12 15:16:04 +0100

    Feat: Added swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(`api/v1`);
+  app.setGlobalPrefix(`api/v${process.env.VERSION}`);
+  const options = new DocumentBuilder()
+    .setTitle('Tss Authentication Service')
+    .setDescription('Api ofr Tss authentication service')
+    .setVersion('1.0')
+    .addTag('Authentication, tss dev')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(`api/v${process.env.VERSION}/docs`, app, document);
+
   let port = process.env.HOST_PORT || process.env.PORT || 3000;
-   app.listen(port).then(() => {
+  app.listen(port).then(() => {
     console.log(`Starting application on port ${port}`);
     console.log(`Url:: ${process.env.DOMAIN}:${port}/api/v${process.env.VERSION}`);
   });
