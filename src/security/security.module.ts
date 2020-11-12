2f9343e63232893426314a36dad3d37089a59e09
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-11-12 14:53:06 +0100

    feat: Generate private refresh token with access token.

---
+++ src/security/security.module.ts
@@ -0,0 +1,60 @@
+import { Module } from '@nestjs/common';
+import { RequestMetaDataInterceptor } from './interceptors/request-meta-data.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthJwsGenerator } from './core/auth-jwt-generator.core';
+import { RefreshTokenGeneratorCore } from './core/refresh-token-generator.core';
+import { AccessTokenGeneratorCore } from './core/access-token-generator.core';
+import { ACCESSKEYGENERATOR, REFRESHKEYGENERATOR } from './constants';
+import { Connection } from 'typeorm';
+import { KeyGenerator } from './contracts/key-generator.contracts';
+import { KeyGeneratorCore } from './core/key-generator.core';
+
+const refreshTokenGenerator = {
+  provide: REFRESHKEYGENERATOR,
+  useExisting: RefreshTokenGeneratorCore,
+  useFactory: async (connection: Connection, keyGenerator: KeyGenerator) => {
+    let refreshTokenGeneratorCore = new RefreshTokenGeneratorCore(new AuthJwsGenerator(), connection, keyGenerator);
+    await refreshTokenGeneratorCore.onApplicationBootstrap();
+    return refreshTokenGeneratorCore;
+  },
+  inject: [Connection, KeyGenerator],
+};
+
+const accessKeyGeneratorCore = {
+  provide: ACCESSKEYGENERATOR,
+  useExisting: RefreshTokenGeneratorCore,
+  useFactory: async (connection: Connection, keyGenerator: KeyGenerator) => {
+    let accessTokenGeneratorCore = new AccessTokenGeneratorCore(new AuthJwsGenerator(), connection, keyGenerator);
+    await accessTokenGeneratorCore.onApplicationBootstrap();
+    return accessTokenGeneratorCore;
+  },
+  inject: [Connection, KeyGenerator],
+};
+
+
+const keyGenerator = {
+  provide: KeyGenerator,
+  useExisting: KeyGeneratorCore,
+
+};
+
+@Module({
+  providers: [
+    RequestMetaDataInterceptor,
+    {
+      provide: APP_INTERCEPTOR,
+      useExisting: RequestMetaDataInterceptor,
+    },
+    KeyGeneratorCore,
+    keyGenerator,
+    refreshTokenGenerator,
+    accessKeyGeneratorCore,
+  ],
+  exports: [
+    refreshTokenGenerator,
+    accessKeyGeneratorCore,
+  ],
+
+})
+export class SecurityModule {
+}
