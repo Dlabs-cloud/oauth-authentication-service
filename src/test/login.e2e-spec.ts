fd1b26acf31cb423fdda6fc28f63d7ea6ebdeb41
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-11-18 19:23:59 +0100

    feat: Added login test.

---
+++ src/test/login.e2e-spec.ts
@@ -0,0 +1,68 @@
+import { TestingModule } from '@nestjs/testing';
+import { baseTestingModule, signUpUser } from './test-utils';
+import { ValidatorTransformerPipe } from '@tss/common/pipes/validator-transformer.pipe';
+import { Connection, getConnection } from 'typeorm';
+import { INestApplication } from '@nestjs/common';
+import { PortalUserRegistrationService } from '../service/portal-user-registration.service';
+import * as request from 'supertest';
+import { LoginRequest } from '../data/request/login.request';
+import * as faker from 'faker';
+
+describe('Login e2e', () => {
+  let applicationContext: INestApplication;
+  let connection: Connection;
+  let user: { identifier, password };
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await baseTestingModule().compile();
+    applicationContext = moduleRef.createNestApplication();
+    applicationContext.useGlobalPipes(new ValidatorTransformerPipe());
+    await applicationContext.init();
+    connection = getConnection();
+    const userRegistrationService = moduleRef.get<PortalUserRegistrationService>(PortalUserRegistrationService);
+    user = await signUpUser(userRegistrationService);
+  });
+
+
+  it('Test that an invalid user cannot login', () => {
+    const loginRequest: LoginRequest = {
+      identifier: faker.internet.email(),
+      password: user.password,
+    };
+    const url = '/login';
+    return request(applicationContext.getHttpServer())
+      .post(url)
+      .send(loginRequest);
+  });
+
+  it('Test that a valid user can login and get tokens', () => {
+    const loginRequest: LoginRequest = {
+      identifier: user.identifier,
+      password: user.password,
+    };
+    const url = '/login';
+    return request(applicationContext.getHttpServer())
+      .post(url)
+      .send(loginRequest)
+      .expect(200).then(response => {
+        let payload = response.body.data;
+        expect(payload).toBeDefined();
+        expect(payload.token_type).toEqual('Bearer');
+        expect(payload.displayName).toBeDefined();
+        expect(payload.firstName).toBeDefined();
+        expect(payload.lastName).toBeDefined();
+        expect(payload.gender).toBeDefined();
+        expect(payload.emailAddresses).toBeDefined();
+        expect(payload.phoneNumbers).toBeDefined();
+        expect(payload.refresh_token).toBeDefined();
+        expect(payload.access_token).toBeDefined();
+        expect(payload.secondsTillExpiry).toBeDefined();
+        expect(payload.expires_at).toBeDefined();
+      });
+
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await applicationContext.close();
+  });
+});
--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -2,6 +2,13 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from '../app.module';
 import { ServiceImplModule } from '../service-impl/service-impl.module';
 import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
+import { UserRegistrationApiRequest } from '../data/request/user-registration-api.request';
+import * as faker from 'faker';
+import { Gender } from '../domain/constants/gender.constant';
+import { PortalUserRegistrationServiceImpl } from '../service-impl/portal-user-registration.service-impl';
+import { RequestMetaData } from '../security/data/request-meta-data.dto';
+import { PortalUserRegistrationService } from '../service/portal-user-registration.service';
+import { PortalUser } from '../domain/entity/portal-user.entity';
 
 export const mockSendEmail = () => jest.fn().mockImplementation((sendEmailOptions: ISendMailOptions) => {
   return Promise.resolve('Email has been sent successfully');
@@ -15,4 +22,38 @@ export function baseTestingModule() {
     .useValue({
       sendMail: mockSendEmail(),
     });
+}
+
+export function signUpUser(registrationService: PortalUserRegistrationService) {
+  const requestPayload: UserRegistrationApiRequest = {
+    displayName: faker.name.lastName(),
+    email: faker.internet.email(),
+    firstName: faker.name.firstName(),
+    gender: Gender.FEMALE,
+    isPasswordUpdateRequired: true,
+    lastName: faker.name.lastName(),
+    otherNames: faker.name.firstName(),
+    password: faker.random.uuid(),
+    phoneNumber: `${faker.phone.phoneNumber('+234#########')}`,
+  };
+  const requestMetaData: RequestMetaData = {
+    get ipAddress(): string {
+      return '127.0.0.1';
+    },
+    get localHost(): boolean {
+      return true;
+    },
+    tokenExpired: false,
+    get userAgent(): string {
+      return 'Test: Environment';
+    },
+
+  } as RequestMetaData;
+  return registrationService.register(requestPayload, requestMetaData).then(user => {
+    return Promise.resolve({
+      identifier: requestPayload.email,
+      password: requestPayload.password,
+    });
+  });
+
 }
