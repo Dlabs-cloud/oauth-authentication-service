54fbe1363d7af40020eea71a645a9e2238d1f89b
Oluwatobi Adenekan <tobi>
2021-07-13 13:57:35 +0100

    fix up.

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -1,19 +1,19 @@
 import { Body, Controller, HttpStatus, Inject, Post } from '@nestjs/common';
-import { LoginRequest } from '../data/request/login.request';
-import { RequestMetaData } from '../security/data/request-meta-data.dto';
-import { RequestMetaDataContext } from '../security/decorators/request-meta-data.decorator';
 import { LoginAuthenticationService } from '../service/login-authentication.service';
+import { AccessTokenApiResponseHandler } from './handler/access-token-api-response.handler';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { AccessTokenApiResponse } from '../data/response/access-token-api.response';
+import { LoginRequest } from '../data/request/login.request';
+import { RequestMetaDataContext } from '../security/decorators/request-meta-data.decorator';
+import { RequestMetaData } from '../security/data/request-meta-data.dto';
 import { AuthenticationResponseType } from '../domain/constants/authentication-response-type,constant';
 import { ErrorResponseException } from '@tss/common/exceptions/error-response.exception';
 import { ApiResponseDto } from '@tss/common/data/api.response.dto';
-import { AccessTokenApiResponseHandler } from './handler/access-token-api-response.handler';
 import { Public } from '../security/decorators/public.decorator';
-import { ApiOkResponse } from '@nestjs/swagger';
-import { AccessTokenApiResponse } from '../data/response/access-token-api.response';
 
 @Controller()
 @Public()
-export class LoginAuthenticationController {
+export class LoginController {
   constructor(@Inject(LoginAuthenticationService) private readonly loginAuthenticationService: LoginAuthenticationService,
               protected readonly accessTokenApiResponseHandler: AccessTokenApiResponseHandler) {
   }
@@ -21,7 +21,7 @@ export class LoginAuthenticationController {
   @Post('/login')
   @ApiOkResponse({ type: AccessTokenApiResponse })
   async login(@Body() loginRequest: LoginRequest, @RequestMetaDataContext() requestMetaData: RequestMetaData) {
-    let portalUserAuthentication = await this.loginAuthenticationService.getAuthenticationResponse(loginRequest, requestMetaData);
+    const portalUserAuthentication = await this.loginAuthenticationService.getAuthenticationResponse(loginRequest, requestMetaData);
     if (portalUserAuthentication.responseType !== AuthenticationResponseType.SUCCESSFUL) {
       if (portalUserAuthentication.responseType == AuthenticationResponseType.UNKNOWN_ACCOUNT) {
         throw new ErrorResponseException(HttpStatus.UNAUTHORIZED, 'UnAuthorized ');
